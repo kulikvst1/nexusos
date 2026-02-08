f371b97bd2b549120e5977a9d10647b488b54f42
kulikvst1 <153820830+kulikvst1@users.noreply.github.com>
2026-02-08 09:01:29 +0200

    Delete NEXUS KONTROL OS/NXUS OS CONTROL/Source directory

--- NEXUS KONTROL OS/NXUS OS CONTROL/Source/IPitchDetector.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-struct PitchResult { float hz; float confidence; };
-
-struct IPitchDetector {
-    virtual ~IPitchDetector() = default;
-    virtual void prepare(int bufferSize, double sampleRate) = 0;
-    virtual PitchResult process(const float* frame, int size) = 0;
-};
